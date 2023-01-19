4415a9727364564b9ee97a052903b26e34e45f0e
StormyFrolic <stormethan10@gmail.com>
2023-01-18 16:25:40 -0800

    Update setProxy.js

--- client/src/setProxy.js
+++ client/src/setProxy.js
@@ -2,7 +2,7 @@ const { createProxyMiddleware } = require("http-proxy-middleware")
 
 module.exports = function (app) {
   app.use(
-    "/user",
+    "/",
     createProxyMiddleware({
       target: "https://poke-catch.herokuapp.com/",
       changeOrigin: true,
